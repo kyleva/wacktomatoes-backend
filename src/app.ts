468b62585d5aeeb1a1fdf0acd5f164dd1bd6f9cb
Kyle Stearns <kyleva@gmail.com>
2020-05-01 00:36:19 -0400

    another shot @ cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,17 @@ createConnection().then(() => {
   const app: Express = express();
   const port: number = Number(process.env.PORT) || 3030;
 
-  app.options('*', cors());
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+    );
+    next();
+  });
+
   // app.use(helmet());
   app.use(bodyParser.json());
   app.use('/', routes);
