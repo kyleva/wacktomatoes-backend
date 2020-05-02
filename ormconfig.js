29d9bb25a5b8f7fd72fcd66b33d2f22876941db1
Kyle Stearns <kyleva@gmail.com>
2020-05-01 20:34:51 -0400

    changing around script structure

--- ormconfig.js
+++ ormconfig.js
@@ -7,11 +7,7 @@ module.exports = {
   database: process.env.DB_NAME || 'wacktomatoes',
   synchronize: true,
   logging: false,
-  entities: [
-    'build/domains/*/entity.js',
-    'src/**/*.entity{.ts,.js}',
-    'domains/*/entity.js',
-  ],
+  entities: ['build/domains/*/entity.js', 'src/**/*.entity{.ts,.js}'],
   migrations: ['build/migrations/**/*.js', 'src/migrations/**/*.js'],
   subscribers: ['build/subscribers/**/*.js', 'src/subscribers/**/*.js'],
   cli: {
